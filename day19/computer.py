aac4527424fbdc4e4260dd4f19d1afe00a5c0529
Brian Perrett <perrettbrian@gmail.com>
2020-01-29 22:56:31 -0700

    continue day 19

--- day19/computer.py
+++ day19/computer.py
@@ -1,91 +1,104 @@
 class Computer:
-    def __init__(self, register=None, instructions=None):
-        self.register = register[:] if register else []
+    def __init__(self, register=None, instructions=None, pointer=0):
+        self.register = register[:] if register else [0] * 6
+        self.pointer = pointer
         self.instructions = instructions
-        self.i
-        self.m = [
-            self.addr,
-            self.addi,
-            self.mulr,
-            self.muli,
-            self.borr,
-            self.bori,
-            self.banr,
-            self.bani,
-            self.setr,
-            self.seti,
-            self.gtir,
-            self.gtri,
-            self.gtrr,
-            self.eqir,
-            self.eqri,
-            self.eqrr
-        ]
+        self.m = {
+            'addr': self.addr,
+            'addi': self.addi,
+            'mulr': self.mulr,
+            'muli': self.muli,
+            'borr': self.borr,
+            'bori': self.bori,
+            'banr': self.banr,
+            'bani': self.bani,
+            'setr': self.setr,
+            'seti': self.seti,
+            'gtir': self.gtir,
+            'gtri': self.gtri,
+            'gtrr': self.gtrr,
+            'eqir': self.eqir,
+            'eqri': self.eqri,
+            'eqrr': self.eqrr
+        }
+
+    def run_until_finished(self):
+        while self.get_pointer() >= 0 and self.get_pointer() < len(self.instructions):
+            # print(self.register)
+            # print(self.instructions[self.get_pointer()])
+            # input()
+            self.run()
+
+    def get_pointer(self):
+        return self.register[self.pointer]
 
     def run(self):
-        self.m[self.instruction[0]]()
+        index = self.get_pointer()
+        instruction = self.instructions[index]
+        self.m[instruction[0]](instruction)
+        self.register[self.pointer] += 1
 
-    def addr(self):
-        ra, rb, rc = self.instruction[1: 4]
+    def addr(self, instruction):
+        ra, rb, rc = instruction[1: 4]
         self.register[rc] = self.register[ra] + self.register[rb]
 
-    def addi(self):
-        ra, b, rc = self.instruction[1: 4]
+    def addi(self, instruction):
+        ra, b, rc = instruction[1: 4]
         self.register[rc] = self.register[ra] + b
 
-    def mulr(self):
-        ra, rb, rc = self.instruction[1: 4]
+    def mulr(self, instruction):
+        ra, rb, rc = instruction[1: 4]
         self.register[rc] = self.register[ra] * self.register[rb]
 
-    def muli(self):
-        ra, b, rc = self.instruction[1: 4]
+    def muli(self, instruction):
+        ra, b, rc = instruction[1: 4]
         self.register[rc] = self.register[ra] * b
 
-    def borr(self):
-        ra, rb, rc = self.instruction[1: 4]
+    def borr(self, instruction):
+        ra, rb, rc = instruction[1: 4]
         self.register[rc] = self.register[ra] | self.register[rb]
 
-    def bori(self):
-        ra, b, rc = self.instruction[1: 4]
+    def bori(self, instruction):
+        ra, b, rc = instruction[1: 4]
         self.register[rc] = self.register[ra] | b
 
-    def banr(self):
-        ra, rb, rc = self.instruction[1: 4]
+    def banr(self, instruction):
+        ra, rb, rc = instruction[1: 4]
         self.register[rc] = self.register[ra] & self.register[rb]
 
-    def bani(self):
-        ra, b, rc = self.instruction[1: 4]
+    def bani(self, instruction):
+        ra, b, rc = instruction[1: 4]
         self.register[rc] = self.register[ra] & b
 
-    def setr(self):
-        ra, _, rc = self.instruction[1: 4]
+    def setr(self, instruction):
+        ra, _, rc = instruction[1: 4]
         self.register[rc] = self.register[ra]
 
-    def seti(self):
-        a, _, rc = self.instruction[1: 4]
+    def seti(self, instruction):
+        a, _, rc = instruction[1: 4]
         self.register[rc] = a
 
-    def gtir(self):
-        a, rb, rc = self.instruction[1: 4]
+    def gtir(self, instruction):
+        a, rb, rc = instruction[1: 4]
         self.register[rc] = 1 if a > self.register[rb] else 0
 
-    def gtri(self):
-        ra, b, rc = self.instruction[1: 4]
+    def gtri(self, instruction):
+        ra, b, rc = instruction[1: 4]
         self.register[rc] = 1 if self.register[ra] > b else 0
 
-    def gtrr(self):
-        ra, rb, rc = self.instruction[1: 4]
+    def gtrr(self, instruction):
+        ra, rb, rc = instruction[1: 4]
         self.register[rc] = 1 if self.register[ra] > self.register[rb] else 0
 
-    def eqir(self):
-        a, rb, rc = self.instruction[1: 4]
+    def eqir(self, instruction):
+        a, rb, rc = instruction[1: 4]
         self.register[rc] = 1 if a == self.register[rb] else 0
 
-    def eqri(self):
-        ra, b, rc = self.instruction[1: 4]
+    def eqri(self, instruction):
+        ra, b, rc = instruction[1: 4]
         self.register[rc] = 1 if self.register[ra] == b else 0
 
-    def eqrr(self):
-        ra, rb, rc = self.instruction[1: 4]
+    def eqrr(self, instruction):
+        ra, rb, rc = instruction[1: 4]
         self.register[rc] = 1 if self.register[ra] == self.register[rb] else 0
 
--- day19/day19.py
+++ day19/day19.py
@@ -1,7 +1,29 @@
+from computer import Computer
 import pathlib
 
 cwd = pathlib.Path(__file__).parent.absolute()
 dpath = pathlib.PurePath(cwd, 'data')
+tpath = pathlib.PurePath(cwd, 'test')
 
 
+if __name__ == '__main__':
+    with open(dpath, 'r') as f:
+        instructions = []
+        for line in f:
+            line = line.strip()
+            if '#ip' in line:
+                pointer = int(line.split()[-1])
+            else:
+                line = line.split()
+                line = [line[0]] + list(map(int, line[1:]))
+                instructions.append(line)
+
+        # computer = Computer(instructions=list(instructions), pointer=pointer)
+        # computer.run_until_finished()
+        # print(computer.register)
+
+        # computer = Computer(instructions=list(instructions), pointer=pointer, register=[0, 1, 0, 10551263, 6, 10551264])
+        computer = Computer(instructions=list(instructions), pointer=pointer, register=[1, 0, 0, 0, 0, 0])
+        computer.run_until_finished()
+        print(computer.register)
 
